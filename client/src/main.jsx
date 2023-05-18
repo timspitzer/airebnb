22176ce6a884fc2d161cd39d400d1cfc8116beea
Tim Spitzer <tim.spitzer10@gmail.com>
2023-05-18 15:44:33 -0500

    Added css reset

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,8 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
+import "@unocss/reset/sanitize/sanitize.css";
+import "@unocss/reset/sanitize/assets.css";
 import "virtual:uno.css";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
