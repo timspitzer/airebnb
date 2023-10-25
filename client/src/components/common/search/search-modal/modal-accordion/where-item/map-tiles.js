a77b44683496b847a65a51f2ab86683dbff46c2c
Tim Spitzer <tim.spitzer10@gmail.com>
2023-10-25 13:45:29 +0200

    Renamed destination constant

---
+++ client/src/components/common/search/search-modal/modal-accordion/where-item/map-tiles.js
@@ -0,0 +1,38 @@
+export const MAP_TILES = [
+  {
+    img: "https://a0.muscache.com/pictures/f9ec8a23-ed44-420b-83e5-10ff1f071a13.jpg",
+    name: "",
+  },
+  {
+    img: "https://a0.muscache.com/im/pictures/7b5cf816-6c16-49f8-99e5-cbc4adfd97e2.jpg?im_w=320",
+    name: "Europe",
+  },
+  {
+    img: "https://a0.muscache.com/im/pictures/a940f971-4ac1-413b-90dd-bd55965b215e.jpg?im_w=320",
+    name: "Mexico",
+  },
+  {
+    img: "https://a0.muscache.com/im/pictures/4e762891-75a3-4fe1-b73a-cd7e673ba915.jpg?im_w=320",
+    name: "United States",
+  },
+  {
+    img: "https://a0.muscache.com/im/pictures/0907be0d-30dd-47c0-a0d5-cbef355fbadf.jpg?im_w=320",
+    name: "Colombia",
+  },
+  {
+    img: "https://a0.muscache.com/im/pictures/c61d10f5-4ab3-4d16-bed7-0449e15c8277.jpg?im_w=320",
+    name: "Caribbean",
+  },
+  {
+    img: "https://a0.muscache.com/im/pictures/86307179-60ee-4a7e-a4cb-c32d511bee0b.jpg?im_w=320",
+    name: "Argentina",
+  },
+  {
+    img: "https://a0.muscache.com/im/pictures/66a7dcd7-607a-48b0-9962-4e14179615c0.jpg?im_w=320",
+    name: "Central America",
+  },
+  {
+    img: "https://a0.muscache.com/im/pictures/a0fd6dfc-6bec-4abb-850e-9ab78ed7bf37.jpg?im_w=320",
+    name: "Spain",
+  },
+];
