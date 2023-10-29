fdb30031b13933bdbd40c6e83f54fb73e4443565
Tim Spitzer <tim.spitzer10@gmail.com>
2023-10-29 09:31:39 +0100

    Removed timezone "correction"

--- client/src/components/common/search/search-modal/modal-accordion/when-item/datepicker/datepicker.jsx
+++ client/src/components/common/search/search-modal/modal-accordion/when-item/datepicker/datepicker.jsx
@@ -5,20 +5,14 @@ export function DatePicker() {
   const { formData, updateFormData } = useContext(FormDataContext);
 
   function onStartDateChange(event) {
-    const date = new Date(
-      // event.target.valueAsNumber + new Date().getTimezoneOffset() * 60 * 1000
-      event.target.valueAsNumber
-    );
+    const date = new Date(event.target.valueAsNumber);
     updateFormData({
       startDate: date,
     });
   }
 
   function onEndDateChange(event) {
-    const date = new Date(
-      // event.target.valueAsNumber + new Date().getTimezoneOffset() * 60 * 1000
-      event.target.valueAsNumber
-    );
+    const date = new Date(event.target.valueAsNumber);
     updateFormData({
       endDate: date,
     });
