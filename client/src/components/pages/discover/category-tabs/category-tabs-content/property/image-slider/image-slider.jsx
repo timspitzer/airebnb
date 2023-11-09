3d08eb188fb9884d1c6c0beb133507f7ca53578b
Tim Spitzer <tim.spitzer10@gmail.com>
2023-11-09 15:20:51 +0100

    Image Slider Done

--- client/src/components/pages/discover/category-tabs/category-tabs-content/property/image-slider/image-slider.jsx
+++ client/src/components/pages/discover/category-tabs/category-tabs-content/property/image-slider/image-slider.jsx
@@ -1,27 +1,40 @@
-import { useEffect, useState } from "react";
+import { useRef } from "react";
 
 export function ImageSlider({ images, areButtonsShown }) {
-  const [activeImage, setActiveImage] = useState(0);
+  const sliderRef = useRef(null);
 
-  const handleArrowClick = (event) => {
-    event.preventDefault();
-    console.log(event);
-    console.log("test");
+  const handleArrowClick = (direction) => {
+    if (sliderRef.current) {
+      const elementWidth = sliderRef.current.offsetWidth;
+      const scrollDistance =
+        direction === "right" ? elementWidth : -elementWidth;
+      sliderRef.current.scrollBy({
+        top: 0,
+        left: scrollDistance,
+        behavior: "smooth",
+      });
+    }
   };
 
   return (
-    <div className="w-75 h-71 grid-cols-[auto 1fr auto] rounded-3 grid">
-      <div className="scrollbar-hide grid-col-[1/4] grid-row-[1] flex items-center overflow-hidden rounded-[inherit]">
+    <div className="w-75 h-71 grid-cols-[auto 1fr auto] rounded-3 bg-gray grid overflow-hidden">
+      {/* IMAGES */}
+      <div
+        ref={sliderRef}
+        className="grid-col-[1/4] grid-row-[1] snap-type-x-mandatory flex overflow-x-scroll scroll-smooth"
+      >
         {images.map((image, i) => (
           <img
-            className="h-full w-full shrink-0 object-cover"
             key={i}
             src={image}
+            className="h-full w-full flex-shrink-0 snap-center object-cover"
           ></img>
         ))}
       </div>
+      {/* BUTTON LEFT */}
       <div className="grid-col-[1] grid-row-[1] flex items-center">
         <button
+          onClick={() => handleArrowClick("left")}
           className={`ml-3 rounded-full border-[0.5px] border-solid border-[#d8d8d8] bg-[#ffffff] p-1 ${
             areButtonsShown ? "block" : "hidden"
           }`}
@@ -29,8 +42,10 @@ export function ImageSlider({ images, areButtonsShown }) {
           <div className="i-radix-icons:chevron-left text-xl"></div>
         </button>
       </div>
-      <div className="grid-col-[3] grid-row-[1] flex items-center text-center">
+      {/* BUTTON RIGHT  */}
+      <div className="grid-col-[3] grid-row-[1] flex items-center">
         <button
+          onClick={() => handleArrowClick("right")}
           className={`mr-3 rounded-full border-[0.5px] border-solid border-[#d8d8d8] bg-[#ffffff] p-1 ${
             areButtonsShown ? "block" : "hidden"
           }`}
