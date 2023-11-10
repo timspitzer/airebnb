1e269e7bf956440f7a7aca72677b8efcdcdd754c
Tim Spitzer <tim.spitzer10@gmail.com>
2023-11-10 10:38:59 +0100

    fix: react v18

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,19 @@
 import "virtual:uno.css";
 import "./index.css";
 
-import App from "./app.jsx";
-import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import React from "react";
+import { render } from "react-dom";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+// import App from "./app.jsx";
+// import ReactDOM from "react-dom/client";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+
+const container = document.getElementById("root");
+render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  container
+);
