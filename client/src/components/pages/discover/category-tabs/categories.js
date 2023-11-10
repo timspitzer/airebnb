172e11e84ab741313a4aa31752851808e97d173a
Tim Spitzer <tim.spitzer10@gmail.com>
2023-11-10 13:08:49 +0100

    fix: added base url

--- client/src/components/pages/discover/category-tabs/categories.js
+++ client/src/components/pages/discover/category-tabs/categories.js
@@ -1,86 +1,86 @@
 export const CATEGORIES = [
   {
     name: "Rooms",
-    iconUrl: "../../../../../../images/category-icons/rooms-icon.png",
+    iconUrl: "/airebnb/images/category-icons/rooms-icon.png",
   },
   {
     name: "Beachfront",
-    iconUrl: "../../../../../../images/category-icons/beachfront-icon.png",
+    iconUrl: "/airebnb/images/category-icons/beachfront-icon.png",
   },
   {
     name: "Cabins",
-    iconUrl: "../../../../../../images/category-icons/cabins-icon.png",
+    iconUrl: "/airebnb/images/category-icons/cabins-icon.png",
   },
   {
     name: "Amazing Pools",
-    iconUrl: "../../../../../../images/category-icons/amazing-pools-icon.png",
+    iconUrl: "/airebnb/images/category-icons/amazing-pools-icon.png",
   },
   {
     name: "Countryside",
-    iconUrl: "../../../../../../images/category-icons/countryside-icon.png",
+    iconUrl: "/airebnb/images/category-icons/countryside-icon.png",
   },
   {
     name: "Amazing Views",
-    iconUrl: "../../../../../../images/category-icons/amazing-views-icon.png",
+    iconUrl: "/airebnb/images/category-icons/amazing-views-icon.png",
   },
   {
     name: "Trending",
-    iconUrl: "../../../../../../images/category-icons/trending-icon.png",
+    iconUrl: "/airebnb/images/category-icons/trending-icon.png",
   },
   {
     name: "Mansions",
-    iconUrl: "../../../../../../images/category-icons/mansions-icon.png",
+    iconUrl: "/airebnb/images/category-icons/mansions-icon.png",
   },
   {
     name: "OMG!",
-    iconUrl: "../../../../../../images/category-icons/omg-icon.png",
+    iconUrl: "/airebnb/images/category-icons/omg-icon.png",
   },
   {
     name: "Tiny Houses",
-    iconUrl: "../../../../../../images/category-icons/tiny-houses-icon.png",
+    iconUrl: "/airebnb/images/category-icons/tiny-houses-icon.png",
   },
   {
     name: "Castles",
-    iconUrl: "../../../../../../images/category-icons/castles-icon.png",
+    iconUrl: "/airebnb/images/category-icons/castles-icon.png",
   },
   {
     name: "Skiing",
-    iconUrl: "../../../../../../images/category-icons/skiing-icon.png",
+    iconUrl: "/airebnb/images/category-icons/skiing-icon.png",
   },
   {
     name: "Iconic Cities",
-    iconUrl: "../../../../../../images/category-icons/iconic-cities-icon.png",
+    iconUrl: "/airebnb/images/category-icons/iconic-cities-icon.png",
   },
   {
     name: "National parks",
-    iconUrl: "../../../../../../images/category-icons/national-parks-icon.png",
+    iconUrl: "/airebnb/images/category-icons/national-parks-icon.png",
   },
   {
     name: "Vineyards",
-    iconUrl: "../../../../../../images/category-icons/vineyards-icon.png",
+    iconUrl: "/airebnb/images/category-icons/vineyards-icon.png",
   },
   {
     name: "Tropical",
-    iconUrl: "../../../../../../images/category-icons/tropical-icon.png",
+    iconUrl: "/airebnb/images/category-icons/tropical-icon.png",
   },
   {
     name: "Caves",
-    iconUrl: "../../../../../../images/category-icons/caves-icon.png",
+    iconUrl: "/airebnb/images/category-icons/caves-icon.png",
   },
   {
     name: "Houseboats",
-    iconUrl: "../../../../../../images/category-icons/houseboats-icon.png",
+    iconUrl: "/airebnb/images/category-icons/houseboats-icon.png",
   },
   {
     name: "Farms",
-    iconUrl: "../../../../../../images/category-icons/farms-icon.png",
+    iconUrl: "/airebnb/images/category-icons/farms-icon.png",
   },
   {
     name: "Lake",
-    iconUrl: "../../../../../../images/category-icons/lake-icon.png",
+    iconUrl: "/airebnb/images/category-icons/lake-icon.png",
   },
   {
     name: "Design",
-    iconUrl: "../../../../../../images/category-icons/design-icon.png",
+    iconUrl: "/airebnb/images/category-icons/design-icon.png",
   },
 ];
