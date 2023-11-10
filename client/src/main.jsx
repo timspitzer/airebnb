73490ca30076df6122cd16d3ca4595cdc8b7b4ff
Tim Spitzer <tim.spitzer10@gmail.com>
2023-11-10 10:30:08 +0100

    fix: changed import "App.jsx" to "app.jsx"

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "virtual:uno.css";
 import "./index.css";
+
+import App from "./app.jsx";
+import ReactDOM from "react-dom/client";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
