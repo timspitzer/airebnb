32761d34800343c00060290411b54ee932f69972
Tim Spitzer <tim.spitzer10@gmail.com>
2023-11-10 10:42:40 +0100

    chore: removed old code

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,12 +5,8 @@ import App from "./app.jsx";
 import React from "react";
 import { render } from "react-dom";
 
-// import App from "./app.jsx";
-// import ReactDOM from "react-dom/client";
-
-// ReactDOM.createRoot(document.getElementById("root")).render(<App />);
-
 const container = document.getElementById("root");
+
 render(
   <React.StrictMode>
     <App />
